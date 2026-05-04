8bc3699ff969754495258d1b708104e5c83ac978
Tihomir Kehayov <kehayov89@gmail.com>
2026-05-04 12:42:34 +0300

    init

--- init.sql
+++ init.sql
@@ -11,12 +11,12 @@ CREATE TABLE accounts
 
 CREATE TABLE transfers
 (
-    id                     serial4   NOT NULL PRIMARY KEY,
+    id                     serial4      NOT NULL PRIMARY KEY,
     account_id             int4,
     beneficiary_account_id int4,
-    "type"                 varchar(255)   NOT NULL,
+    "type"                 varchar(255) NOT NULL,
     amount                 numeric,
-    "created"              timestamp NOT NULL,
+    "created"              timestamp    NOT NULL,
     modified               timestamp NULL
 );
 
@@ -24,3 +24,7 @@ ALTER TABLE transfers
     ADD CONSTRAINT account_id_fk FOREIGN KEY (account_id) REFERENCES accounts (id);
 ALTER TABLE transfers
     ADD CONSTRAINT beneficiary_account_id_fk FOREIGN KEY (beneficiary_account_id) REFERENCES accounts (id);
+
+INSERT INTO public.accounts (name, iban, status, available_amount, created, modified)
+VALUES ('Petra', 'BG12STSA93000012245611', true, 100, '2026-05-01 19:30:08.323', '2026-05-03 19:40:16.552'),
+       ('Maria', 'BG19STSA93000012245611', false, 20, '2026-05-03 13:41:35.200', '2026-05-03 19:40:16.553');
